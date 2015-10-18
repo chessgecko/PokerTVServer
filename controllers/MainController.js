08ab76f77abab0a37e5a5ef17450dd6aaa12a896
Dave Silin <davidsilin@pal-nat186-12-27.itap.purdue.edu>
2015-10-18 05:41:17 -0400

    a

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -40,7 +40,7 @@ function updateAllPlayersForNewHand(room){
 function makeid(len)
 {
     var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     for( var i=0; i < len; i++ )
         text += possible.charAt(Math.floor(Math.random() * possible.length));
